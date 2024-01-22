58464fd3ba6bc2ebe49ee9dadfcc6a6a979930d1
0x6a69616e <0x6a69616e@gmail.com>
2024-01-21 21:28:39 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+const part1 = require('./modules/part-1.js');
+const part2 = require('./modules/part-2.js');
 
+(async () => {
+  // obtains the KPSDK message
+  const message = await part1();
+  
+  // processes the KPSDK message to return ct (client token), cd (challenge data), and v (version) headers
+  const kpsdk = part2(message);
+  
+  console.log(kpsdk);
+})();
