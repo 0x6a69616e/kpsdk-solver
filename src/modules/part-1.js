51ee22b3c255e97ebdd31d9d1ba8320f4d6bc1fc
0x6a69616e <0x6a69616e@gmail.com>
2024-01-23 20:03:22 -0500

    Update part-1.js
    load the /fp iframe in a blank page and capture the KPSDK.message directly from it

--- src/modules/part-1.js
+++ src/modules/part-1.js
@@ -6,10 +6,22 @@ module.exports = async function () {
   });
   const page = await browser.newPage();
   
-  // using the Vercel AI Playground (sdk.vercel.ai) to generate KPSDK message
-  await page.goto('https://sdk.vercel.ai');
+  await page.goto('about:blank');
   
-  await page.evaluate('window.onmessage=()=>{throw event.data}');
+  await page.evaluate(() => {
+    window.addEventListener('message', ({ data }) => {
+      if (data.startsWith('KPSDK:DONE:')) throw data;
+    });
+    
+    const iframe = document.createElement('iframe');
+    iframe.src = 'https://sdk.vercel.ai/149e9513-01fa-4fb0-aad4-566afd725d1b/2d206a39-8ed7-437e-a3be-862e0f06eea3/fp?x-kpsdk-v=j-0.0.0';
+    
+    iframe.style.width = iframe.style.height = iframe.style.border = '0';
+    iframe.style.display = 'none';
+    
+    document.body.append(iframe);
+  });
+  
   const message = await new Promise(resolve => page.once('pageerror', ({ message }) => resolve(message)));
   
   await browser.close();
