e5a9448076c12e1768435621fb85cd16e3326654
0x6a69616e <0x6a69616e@gmail.com>
2024-02-02 21:56:09 -0600

    Update part-1.js

--- src/modules/part-1.js
+++ src/modules/part-1.js
@@ -10,27 +10,21 @@ module.exports = async function () {
   
   await page.goto('view-source:https://sdk.vercel.ai');
   
-  await page.evaluate(() => {
-    window.addEventListener('message', ({ data }) => {
-      if (data.startsWith('KPSDK:DONE:')) throw data;
-    });
-    
-    const iframe = document.createElement('iframe');
-    iframe.src = 'https://sdk.vercel.ai/149e9513-01fa-4fb0-aad4-566afd725d1b/2d206a39-8ed7-437e-a3be-862e0f06eea3/fp?x-kpsdk-v=j-0.0.0';
-    
-    iframe.style.width = iframe.style.height = iframe.style.border = '0';
-    iframe.style.display = 'none';
-    iframe.id = 'iframe';
-    
-    document.body.append(iframe);
-  });
-  
-  await new Promise(resolve => page.once('pageerror', resolve));
-  
-  await page.evaluate(() => document.getElementById('iframe').src = document.getElementById('iframe').src);
-  
   const
-    message = await new Promise(resolve => page.once('pageerror', ({ message }) => resolve(message))),
+    { _preview: message } = await page.evaluateHandle(() => new Promise(async resolve => {
+      window.addEventListener('message', ({ data }) => {
+        if (data.startsWith('KPSDK:DONE:')) resolve(data);
+      });
+      
+      const iframe = document.createElement('iframe');
+      iframe.src = 'https://sdk.vercel.ai/149e9513-01fa-4fb0-aad4-566afd725d1b/2d206a39-8ed7-437e-a3be-862e0f06eea3/fp?x-kpsdk-v=j-0.0.0';
+      
+      iframe.style.width = iframe.style.height = iframe.style.border = '0';
+      iframe.style.display = 'none';
+      iframe.id = 'iframe';
+      
+      document.body.append(iframe);
+    })),
     endTime = Date.now();
   
   await browser.close();
