22c2ecb85476a124c61a3acf19f1210dbe71ffa4
0x6a69616e <0x6a69616e@gmail.com>
2024-02-11 10:35:34 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,57 @@
-const {
-  close,
-  get_browser,
+const { firefox } = require('playwright');
 
-  get_message
-} = require('./modules/part-1.js');
-const part2 = require('./modules/part-2.js');
+// configuration for the Kasada SDK
+const config = [
+  {
+    path: '/api/generate',
+  },
+  {
+    path: '/api/prompt'
+  }
+]
+
+// endpoints are under the same domain
+.map(endpoint => (endpoint.domain = 'sdk.vercel.ai', endpoint))
+
+// endpoints use the POST method
+.map(endpoint => (endpoint.method = 'POST', endpoint));
 
 
 (async () => {
-  const [ context ] = await get_browser();
-  
-  const KPSDK_message = await get_message('https://sdk.vercel.ai');
-  
-  const KPSDK = part2(...KPSDK_message.data);
-  const cookies = await context.cookies();
+  const browser = await firefox.launch({
+    headless: true
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
-  console.log(KPSDK_message, KPSDK, cookies);
+  // https://stackoverflow.com/questions/75754759/change-navigator-webdriver-with-playwright
+  await page.addInitScript("delete Object.getPrototypeOf(navigator).webdriver");
 
-  await close();
+  await page.goto('view-source:https://sdk.vercel.ai/149e9513-01fa-4fb0-aad4-566afd725d1b/2d206a39-8ed7-437e-a3be-862e0f06eea3');
+
+  await page.evaluate(() => {
+    document.head.outerHTML = document.body.outerHTML = "";
+  });
+
+  await page.addScriptTag({
+    url: 'https://sdk.vercel.ai/149e9513-01fa-4fb0-aad4-566afd725d1b/2d206a39-8ed7-437e-a3be-862e0f06eea3/p.js'
+  });
+
+  // configure the Kasada SDK and wait until it's ready; capture the KPSDK.message for analysis
+  const KPSDK_message = await page.evaluate(config => {
+    return new Promise(res => {
+      let KPSDK_message;
+
+      window.addEventListener('message', ({
+        data
+      }) => !data.startsWith('KPSDK:DONE:') || (KPSDK_message = data));
+      window.addEventListener('kpsdk-ready', () => !KPSDK.isReady() || res(KPSDK_message));
+      window.KPSDK.configure(config);
+    });
+  }, config);
+
+  // at this point, the Kasada SDK is ready for use!
+  console.log(`\nready on ${new Date(Date.now()).toLocaleString()}!\n${KPSDK_message}\n`);
+
+  await browser.close();
 })();
