52635f2f08010e6864731efb84ce977e333e4662
0x6a69616e <0x6a69616e@gmail.com>
2024-04-06 11:16:23 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,107 +7,120 @@ function nameFunction(name, body) {
   } [name]
 }
 
-function makeId(len = 32) {
-  return Array.from(Array(len)).map(x => Math.random().toString(16).slice(2, 3)).join('');
+function make_id(len = 32) {
+  return Array.from(Array(len)).map(() => Math.random().toString(16).slice(2, 3)).join('');
 }
 
 function build(config) {
-  return async (context, page_cb) => {
+  const rt = config?.['request-tracing'];
+  const sc = config?.kasada;
+  const ss = config?.['sdk-script'];
+  const {
+    'load-complete': lc,
+    url
+  } = config;
+
+  async function create(context, page_cb) {
     const page = await context.newPage();
-
     typeof page_cb !== 'function' || await page_cb(page);
-    
-    const rt = config?.parent.['request-tracing'];
-    const sdk_config = config?.kasada?.configuration;
-    const sdk_script = config?.kasada?.['sdk-script'];
-    const {
-      'load-complete': lc,
-      url
-    }  = config.parent;
-    
-    await page.goto(await (async () => {
-      if (lc) return url;
 
-      const response = await page.request.get(url);
-      await page.route('*/**', async function handler(route) {
-        await route.fulfill({
-          response,
-          status: 200,
-          body: ''
-        });
-        await page.unroute('*/**', handler);
+    async function launch_kasada() {
+      await page.goto(await (async () => {
+        if (lc) return url;
+
+        const response = await page.request.get(url);
+        return await page.route('*/**', async function router(route) {
+          await route.fulfill({
+            response,
+            status: 200,
+            body: ''
+          });
+          await page.unroute('*/**', router);
+        }),
+        response.url();
+      })(), {
+        waitUntil: 'commit'
       });
-      return response.url();
-    })(), {
-      waitUntil: 'commit'
-    });
-    
-    !sdk_script || await page.addScriptTag(sdk_script);
 
-    const fp_listener = res => !/\/149e9513-01fa-4fb0-aad4-566afd725d1b\/2d206a39-8ed7-437e-a3be-862e0f06eea3\/fp/.test(res.url()) || (async _ => {
-      if (!(await res.body()).length) throw new Error(res.url() + ' responded with no body');
-    })();
+      !ss || await page.addScriptTag(ss);
 
-    page.on('response', fp_listener);
-    const messages = await page.evaluate(config => new Promise(resolve => {
-      let msgs = [];
-      window.addEventListener('message', ({
-        data,
-        origin
-      }) => !data.startsWith('KPSDK:DONE:') || msgs.push({
-        message: data,
-        timing: {
-          ms: performance.now(),
-          ts: Date.now()
-        },
-        origin
-      }));
-      window.addEventListener('kpsdk-ready', () => !KPSDK.isReady() || resolve(msgs), {
-        once: true
+      const fp_regex = /\/149e9513-01fa-4fb0-aad4-566afd725d1b\/2d206a39-8ed7-437e-a3be-862e0f06eea3\/fp/;
+      const fp_endpoint_listener = res => !fp_regex.test(res.url()) || (async () => {
+        if (!(await res.body()).length) throw new Error(res.url() + ' responded with no body');
+      })();
+
+      page.on('response', fp_endpoint_listener);
+
+      const messages = await page.evaluate(config => new Promise(resolve => {
+        let msgs = [];
+        window.addEventListener('message', ({
+          data,
+          origin
+        }) => !data.startsWith('KPSDK:DONE:') || msgs.push({
+          message: data,
+          timing: {
+            ms: performance.now(),
+            ts: Date.now()
+          },
+          origin
+        })), window.addEventListener('kpsdk-ready', () => !KPSDK.isReady() || resolve(msgs), {
+          once: true
+        }), !config || window.KPSDK.configure(config);
+      }), sc);
+
+      return page.removeListener('response', fp_endpoint_listener), messages;
+    }
+
+    function fetch(url, options = {}) {
+      if (sc) {
+        const parsed = new URL(url);
+        const endpoint = sc.find(e => (e.domain == parsed.host) && (e.path == parsed.pathname || e.path.includes('*'))) || (() => {
+          throw new Error('kasada is not configured to intercept requests to ' + url);
+        })();
+
+        options.method || (endpoint.method.includes('*') || (options.method = endpoint.method));
+      }
+
+      return new Promise(async (resolve, {
+        trace_header = 'X-Trace-Id',
+        trace_id = rt ? make_id() : 0..a,
+        fn
+      }) => {
+        await page.route(url, fn = nameFunction(make_id(), async (route, request) => {
+          if (trace_id && (request.headers()[trace_header.toLowerCase()] !== trace_id)) return;
+          resolve({
+            request,
+            route
+          });
+          if (page.isClosed) return;
+          await request.response();
+          await page.unroute(url, fn);
+        }));
+
+        try {
+          await page.evaluate(args => window.fetch(...args), [
+            url,
+            trace_id ? {
+              ...options,
+              headers: {
+                ...options.headers,
+                [trace_header]: trace_id
+              }
+            } : options
+          ]);
+        } catch (_) {};
       });
-      !config || window.KPSDK.configure(config);
-    }), sdk_config), page.removeListener('response', fp_listener);
+    }
 
     return (page.solver = {
-      fetch(url, options = {}) {
-        return new Promise(async (resolve, {
-          trace_id = rt ? makeId() : 0..a,
-          fn
-        }) => {
-          await page.route(url, fn = nameFunction(makeId(), async (route, request) => {
-            if (trace_id && (request.headers()['x-trace-id'] !== trace_id)) return;
-            resolve({
-              request,
-              route
-            });
-            if (page.isClosed) return;
-            await request.response();
-            await page.unroute(url, fn);
-          }));
-  
-          try {
-            await page.evaluate(args => window.fetch(...args), [
-              url,
-              trace_id ? {
-                ...options,
-                headers: {
-                  ...options.headers,
-                  'X-Trace-Id': trace_id
-                }
-              } : options
-            ]);
-          } catch (_) {};
-        });
-      },
-      messages
+      fetch,
+      messages: await launch_kasada(),
     }, page);
   }
-}
 
-function KPSDK_SOLVER(c) {
   return {
-    create: !c || build(c)
-  }
+    create
+  };
 }
 
-export { KPSDK_SOLVER as default };
+export { build as default };
